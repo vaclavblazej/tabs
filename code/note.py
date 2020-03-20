ce21a8931f37254fba2529121d3af3bd0492f800
Vaclav Blazej <blazeva1@fit.cvut.cz>
2020-03-20 01:18:18 +0100

    name conversion, remove superfluous chords

--- code/note.py
+++ code/note.py
@@ -24,51 +24,73 @@ class Chord:
         self.notes = set(res)
 
     def contains(self, other):
-        return len(other-self.notes) == 0:
+        return len(other-self.notes) == 0 and len(self.notes-other) != 0
 
 
 def find(my_chord):
     print('search for',my_chord)
+    tmp = []
     for chord in chords:
         if chord.contains(my_chord):
-            print(chord.name, chord.notes)
+            tmp += [chord]
+    contained = len(tmp)*[True]
+    for i in range(len(tmp)):
+        for j in range(len(tmp)):
+            if i != j and tmp[i].contains(tmp[j].notes):
+                contained[i]=False
+    res = []
+    for i in range(len(tmp)):
+        if contained[i]:
+            res += [tmp[i]]
+    return res
 
+def from_names_to_notes(names):
+    res = []
+    for name in names:
+        res += [INVERSE_NOTES[name]]
+    return res
 
 def main():
     global chords
     chords = []
+    global INVERSE_NOTES
+    INVERSE_NOTES = {}
+    for i in range(NOTES):
+        INVERSE_NOTES[NOTE_NAMES[i]]=i
     for i in range(len(NOTE_NAMES)):
         # https://www.pianochord.org/
         name = NOTE_NAMES[i]
-        chords += [Chord(name + ' major',           i, [0,4,7])]
-        chords += [Chord(name + ' minor',           i, [0,3,7])]
-        chords += [Chord(name + ' dominant 7',      i, [0,4,7,10])]
-        chords += [Chord(name + ' minor 7',         i, [0,3,7,10])]
-        chords += [Chord(name + ' major 7',         i, [0,4,7,11])]
-        chords += [Chord(name + ' minor major 7',   i, [0,3,7,11])]
-        chords += [Chord(name + ' 6',               i, [0,4,7,9])]
-        chords += [Chord(name + ' minor 6',         i, [0,3,7,9])]
-        chords += [Chord(name + ' 6/9',             i, [0,4,7,9,2])]
-        chords += [Chord(name + ' 5',               i, [0,7])]
-        chords += [Chord(name + ' 9',               i, [0,4,7,10,2])]
-        chords += [Chord(name + ' minor 9',         i, [0,3,7,10,2])]
-        chords += [Chord(name + ' major 9',         i, [0,4,7,11,2])]
-        chords += [Chord(name + ' 11',              i, [0,4,7,10,2,5])]
-        chords += [Chord(name + ' minor 11',        i, [0,3,7,10,2,5])]
-        chords += [Chord(name + ' 13',              i, [0,4,7,10,2,5,9])]
-        chords += [Chord(name + ' minor 13',        i, [0,3,7,10,2,5,9])]
-        chords += [Chord(name + ' add 9',           i, [0,4,7,2])]
-        chords += [Chord(name + ' 7-5',             i, [0,4,6,10])]
-        chords += [Chord(name + ' 7+5',             i, [0,4,8,10])]
-        chords += [Chord(name + ' sus2',            i, [0,2,7])]
-        chords += [Chord(name + ' sus4',            i, [0,5,7])]
-        chords += [Chord(name + ' dim',             i, [0,3,6])]
-        chords += [Chord(name + ' dim7',            i, [0,3,6,9])]
-        chords += [Chord(name + ' m7(b5)',          i, [0,3,6,10])]
-        chords += [Chord(name + ' aug',             i, [0,4,8])]
-        chords += [Chord(name + ' aug7',            i, [0,4,8,10])]
+        chords += [Chord(name + ' major',       i, [0,4,7])]
+        chords += [Chord(name + ' minor',       i, [0,3,7])]
+        chords += [Chord(name + ' dominant 7',  i, [0,4,7,10])]
+        chords += [Chord(name + ' minor 7',     i, [0,3,7,10])]
+        chords += [Chord(name + ' maj7',        i, [0,4,7,11])]
+        chords += [Chord(name + ' minor maj7',  i, [0,3,7,11])]
+        chords += [Chord(name + ' 6',           i, [0,4,7,9])]
+        chords += [Chord(name + ' minor 6',     i, [0,3,7,9])]
+        chords += [Chord(name + ' 6/9',         i, [0,4,7,9,2])]
+        chords += [Chord(name + ' 5',           i, [0,7])]
+        chords += [Chord(name + ' 9',           i, [0,4,7,10,2])]
+        chords += [Chord(name + ' minor 9',     i, [0,3,7,10,2])]
+        chords += [Chord(name + ' major 9',     i, [0,4,7,11,2])]
+        chords += [Chord(name + ' 11',          i, [0,4,7,10,2,5])]
+        chords += [Chord(name + ' minor 11',    i, [0,3,7,10,2,5])]
+        chords += [Chord(name + ' 13',          i, [0,4,7,10,2,5,9])]
+        chords += [Chord(name + ' minor 13',    i, [0,3,7,10,2,5,9])]
+        chords += [Chord(name + ' add 9',       i, [0,4,7,2])]
+        chords += [Chord(name + ' 7-5',         i, [0,4,6,10])]
+        chords += [Chord(name + ' 7+5',         i, [0,4,8,10])]
+        chords += [Chord(name + ' sus2',        i, [0,2,7])]
+        chords += [Chord(name + ' sus4',        i, [0,5,7])]
+        chords += [Chord(name + ' dim',         i, [0,3,6])]
+        chords += [Chord(name + ' dim7',        i, [0,3,6,9])]
+        chords += [Chord(name + ' m7(b5)',      i, [0,3,6,10])]
+        chords += [Chord(name + ' aug',         i, [0,4,8])]
+        chords += [Chord(name + ' aug7',        i, [0,4,8,10])]
     print('created list of',len(chords),'chords')
-    find(set([2,5,7]))
+    res = find(set(from_names_to_notes(['D','F','G'])))
+    for chord in res:
+        print(chord.name, chord.notes)
     return 0
 
 if __name__ == '__main__':
