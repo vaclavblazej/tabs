189cf553d00bcd4f9ca17242f81703c52698ca97
Václav Blažej <blazeva1@fit.cvut.cz>
2020-03-23 20:12:07 +0100

    note guitar chords init

--- code/note.py
+++ code/note.py
@@ -5,6 +5,12 @@ import sys
 NOTE_NAMES = ['C','C#','D','D#','E','F','F#','G','G#','A','A#','B']
 NOTES = len(NOTE_NAMES)
 
+def from_names_to_notes(names):
+    res = []
+    for name in names:
+        res += [INVERSE_NOTES[name]]
+    return res
+
 class Chord:
     def __init__(self, name, base, notes):
         self.name = name
@@ -26,6 +32,25 @@ class Chord:
     def contains(self, other):
         return len(other-self.notes) == 0 and len(self.notes-other) != 0
 
+#   0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15
+# e|*--*-----*-----*-----*--*-----*-----*--*-------|
+# B|*--*-----*-----*--*-----*-----*-----*--*-----*-|
+# G|*-----*-----*--*-----*-----*--*-----*-----*----|
+# D|*-----*--*-----*-----*-----*--*-----*-----*--*-|
+# A|*-----*--*-----*-----*--*-----*-----*-----*--*-|
+# E|*--*-----*-----*-----*--*-----*-----*--*-------|
+class Guitar:
+    def __init__(self, base=None, sz=15):
+        self.sz = sz
+        if base==None:
+            base = ['E','A','D','G','H','E']
+        for b in base:
+            pass
+
+    def print_chord(self,chord):
+        for i in range(self.sz):
+            print("  "+str(i+1),end='')
+        print()
 
 def find(my_chord):
     print('search for',my_chord)
@@ -44,12 +69,6 @@ def find(my_chord):
             res += [tmp[i]]
     return res
 
-def from_names_to_notes(names):
-    res = []
-    for name in names:
-        res += [INVERSE_NOTES[name]]
-    return res
-
 def main():
     global chords
     chords = []
@@ -60,6 +79,8 @@ def main():
     for i in range(len(NOTE_NAMES)):
         # https://www.pianochord.org/
         name = NOTE_NAMES[i]
+        chords += [Chord(name + ' scale',       i, [0,2,4,5,7,9,11])]
+        chords += [Chord(name + ' mol scale',   i, [0,2,3,5,7,8,10])]
         chords += [Chord(name + ' major',       i, [0,4,7])]
         chords += [Chord(name + ' minor',       i, [0,3,7])]
         chords += [Chord(name + ' dominant 7',  i, [0,4,7,10])]
@@ -91,6 +112,8 @@ def main():
     res = find(set(from_names_to_notes(['D','F','G'])))
     for chord in res:
         print(chord.name, chord.notes)
+    guitar = Guitar()
+    guitar.print_chord(res)
     return 0
 
 if __name__ == '__main__':
